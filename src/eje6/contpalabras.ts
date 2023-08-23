241aebc50cde3bd9660d431e007ccf87fc256fd0
elena25466551 <elenacastrodm76@gmail.com>
2023-08-23 10:17:40 -0300

    contador palabras: terminado

--- src/eje6/contpalabras.ts
+++ src/eje6/contpalabras.ts
@@ -1,5 +1,6 @@
 export function contpalabras(){
-   let  array:string="hola como estas";
-  let conta= array.split(" ").forEach
-  console.log(conta)
+   let  array:string="hola como estas, mucho gusto";
+  let conta= array.split(" ");
+  console.log("La cantidad de palabbras de la aplicacion es: ",conta.length)
 }
+
